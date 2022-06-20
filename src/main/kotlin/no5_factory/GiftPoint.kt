c364fcfcbb6260954523961ed3abc8fedcfa8634
ikemura23 <banbara.work@gmail.com>
2022-06-20 22:48:23 +0900

    コメント追加

--- src/main/kotlin/no5_factory/GiftPoint.kt
+++ src/main/kotlin/no5_factory/GiftPoint.kt
@@ -5,7 +5,7 @@ package no5_factory
  * 例として、ECサイトで新規入会時に無料お買い物ポイントが付与される、という仕組みを表現している
  *
  * 初期化ロジックの分散を防ぐため、companion objectにFactoryメソッドを用意している。
- *
+ * 今は２パターンしかないが、会員パターンが増えてきたときにFactoryメソッドが効果を発揮する
  */
 data class GiftPoint constructor(private var _value: Int = 0) {
     // ポイントの値の取得
@@ -13,6 +13,7 @@ data class GiftPoint constructor(private var _value: Int = 0) {
         get() = _value
 
     init {
+        // 制限
         if (_value < 0) {
             throw IllegalArgumentException("ポイントが0以上ではありません")
         }
