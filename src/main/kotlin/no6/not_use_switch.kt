7415295cae246d46402261446a36d770438acf95
ikemura23 <banbara.work@gmail.com>
2022-06-22 22:59:06 +0900

    sealdクラスを使っていたパターン追加

--- src/main/kotlin/no6/not_use_switch.kt
+++ src/main/kotlin/no6/not_use_switch.kt
@@ -7,18 +7,22 @@ package no6
 
 fun main() {
     // bad
-    val name = MagicManager().getName(MagitType.fire)
-    println("magic name: $name")
+    val name = MagicManager().getName(MagitType.Fire)
+    println("bad magic name: $name")
 
-    // good
+    // good 1
     val magic = Fire()
-    println("magic name: ${magic.name}")
+    println("good 1 magic name: ${magic.name}")
 
     // good 2
+    val magic2 = Magic2.Fire2
+    println("good 2 magic name: ${magic2.name}")
+
+    // good 3
     val magics = HashMap<MagitType, Magic>()
-    magics[MagitType.fire] = Fire()
-    magics[MagitType.shiden] = Shiden()
-    println("magic name: ${magics[MagitType.fire]?.name}")
+    magics[MagitType.Fire] = Fire()
+    magics[MagitType.Shiden] = Shiden()
+    println("good 3 magic name: ${magics[MagitType.Fire]?.name}")
 }
 
 /**
@@ -35,27 +39,39 @@ interface Magic {
 }
 
 class Fire : Magic {
-    override val name: String
-        get() = "ファイア"
-    override val contMagicPoint: Int
-        get() = 1
-    override val attachPower: Int
-        get() = 2
-    override val costTechnicalPoint: Int
-        get() = 3
+    override val name = "ファイア"
+    override val contMagicPoint = 1
+    override val attachPower = 2
+    override val costTechnicalPoint = 3
 
 }
 
 class Shiden : Magic {
-    override val name: String
-        get() = "紫電"
-    override val contMagicPoint: Int
-        get() = 4
-    override val attachPower: Int
-        get() = 5
-    override val costTechnicalPoint: Int
-        get() = 6
+    override val name = "紫電"
+    override val contMagicPoint = 4
+    override val attachPower = 5
+    override val costTechnicalPoint = 6
+}
 
+private sealed class Magic2 {
+    open val name: String = ""
+    open val contMagicPoint: Int = 0
+    open val attachPower: Int = 0
+    open val costTechnicalPoint: Int = 0
+
+    object Fire2 : Magic2() {
+        override val name = "ファイア"
+        override val contMagicPoint = 1
+        override val attachPower = 2
+        override val costTechnicalPoint = 3
+    }
+
+    object Shiden : Magic2() {
+        override val name = "紫電"
+        override val contMagicPoint = 4
+        override val attachPower = 5
+        override val costTechnicalPoint = 6
+    }
 }
 
 // -----
@@ -72,16 +88,16 @@ private class MagicManager {
     // 名前を返す
     fun getName(magicType: MagitType): String {
         return when (magicType) {
-            MagitType.fire -> "ファイア"
-            MagitType.shiden -> "紫電"
+            MagitType.Fire -> "ファイア"
+            MagitType.Shiden -> "紫電"
         }
     }
 
     // 消費魔法寮を返す
     fun contMagicPoint(magicType: MagitType): Int {
         return when (magicType) {
-            MagitType.fire -> 2
-            MagitType.shiden -> 3
+            MagitType.Fire -> 2
+            MagitType.Shiden -> 3
         }
     }
 }
@@ -89,7 +105,7 @@ private class MagicManager {
 /**
  * 魔法の種類の定義
  */
-enum class MagitType {
-    fire,  // ファイア、炎の魔法
-    shiden // 紫電、雷の魔法
+private enum class MagitType {
+    Fire,  // ファイア、炎の魔法
+    Shiden // 紫電、雷の魔法
 }
